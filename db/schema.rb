2e8a8a9b0b6da1c18edbbf170b8e521b1d355f1b
Ashwini Mandal <ashwini.mandal@gmail.com>
2018-01-23 17:43:19 +0530

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180123100233) do
+ActiveRecord::Schema.define(version: 20180123101224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(version: 20180123100233) do
     t.datetime "updated_at", null: false
     t.string "firstName"
     t.string "lastName"
-    t.float "mobileNumber"
     t.string "image"
     t.bigint "mb"
+    t.bigint "mobileNumber"
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
