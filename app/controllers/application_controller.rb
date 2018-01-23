837b2587fac724e50aa3530931ca00f2b9973fd9
Ashwini Mandal <ashwini.mandal@gmail.com>
2018-01-23 15:39:41 +0530

    after edit done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   protected
    def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:firstName, :lastName, :mobileNumber,:image, :image_cache, :remove_image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:firstName, :lastName, :mobileNumber,:image, :image_cache, :remove_image])
   end
 
   def after_sign_in_path_for(resource)
