51b7646e045176e411c0c6b66f8514780b2782d1
Ashwini Mandal <ashwini.mandal@gmail.com>
2018-02-01 19:21:51 +0530

    removed profile pic

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -3,10 +3,11 @@ class Hotel < ApplicationRecord
 	#has_many :images, as: :imageable
 	has_many :images, as: :imageable, dependent: :destroy
 
-	name_REGEX = /\A[A-Za-z ]+\Z/
-	rating_REGEX=/\A[0-9]+\Z/
+	name_REGEX = /\A[A-Za-z0-9 ]+[A-Za-z ]+[a-zA-Z0-9]+\Z/
+	rating_REGEX=/\A[0-9]{1}+\Z/
+	location_REGEX = /\A[A-Za-z]+\Z/
     validates :name, :presence => true,  :length => { :in => 3..10 },:format => name_REGEX
-    validates :location, :presence => true,  :length => { :in => 3..10 },:format => name_REGEX
+    validates :location, :presence => true,  :length => { :in => 3..10 },:format => location_REGEX
 	 validates :rating, :presence => true
 	validates_numericality_of :rating, :only_integer => true, 
     :greater_than_or_equal_to => 0,
--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -2,4 +2,33 @@ require 'rails_helper'
 
 RSpec.describe Hotel, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  name_REGEX = /\A[A-Za-z0-9 ]+[A-Za-z ]+[a-zA-Z0-9]+\Z/
+	rating_REGEX=/\A[0-9]{1}+\Z/
+	location_REGEX = /\A[A-Za-z]+\Z/
+
+	let(:hotel)  { Hotel.new( name: "Taj",rating:2,location: "solapur")}
+
+	describe "Hotel Name validation" do
+		 name_REGEX = /\A[A-Za-z0-9 ]+[A-Za-z ]+[a-zA-Z0-9]+\Z/
+
+			it " hotel name can't be blank" do
+				hotel.name = nil
+				hotel.save
+				expect(hotel.errors.messages[:name][0]).to eql("can't be blank")
+		   end
+
+		   it "hotel name is invalid.." do
+	    		   hotel.name="123"
+			   hotel.name.match(name_REGEX)
+			   hotel.save
+				expect(hotel.errors.messages[:name]).to eql(["is invalid"])
+		   end
+
+		    it " hotel name valid " do
+		   		   hotel.name="11abc"
+			      hotel.name.match(name_REGEX)
+			expect((hotel.errors.messages[:name]).empty?).to eql(true)
+		   end
+	end
+
 end
