96dff03a54641e1ed3af7809cbb762be974c9d19
Ashwini Mandal <ashwini.mandal@gmail.com>
2018-02-20 11:31:21 +0530

    done with rspec

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'ashumandal96@gmail.com'
   layout 'mailer'
+
+
+  def sample_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Sample Email')
+  end
 end
--- spec/services/booking_spec.rb
+++ spec/services/booking_spec.rb
@@ -1,22 +1,29 @@
 require 'rails_helper'
 
-describe "In-booking" do
-
+describe "booking service" do
+	 let(:params) {{checkinDate:"2/3/2018",checkoutDate:"6/3/2018",roomsList:[52,53]}}
+	 let(:params1) {{checkinDate1:"2/3/2018",checkoutDate1:"6/3/2018",bookingDate1:"20/3/2018",member_id:51}}
 	context "user must be login with valid emailid and password" do
 		context "When user is not logged in "do
-			it "it should redirect to login page" do end
+					it "it should redirect to login page" do
+		   		 	end
 		end
 		context "When user is logged in "do
-			it "it should return login status"do end
-		end
-	end
-	context "it should show room details"do
-		context "it should make payment"do
-			context "When payment done successfully" do
-				it "it  should send booking confirmation mail to user"do end
-			end
-			context "When payment fail" do
-				it "it  should notify fail to make payment"do end
+			context "should check whether room has been already booked or not"do
+				context "when rooms are not book" do
+					it "it should create new booking record and associate corresponding rooms"do
+					  	searchRoomExist=Room.joins(:bookings).where(:bookings=>{checkinDate: params[:checkinDate],checkoutDate: params[:@checkoutDate]},id:params[:roomsList]).uniq
+					  	expect(searchRoomExist.empty?).to eql(true)
+					    newBooking=Booking.create(checkinDate: params1[:checkinDate1],checkoutDate: params1[:checkoutDate1],bookingDate: params1[:bookingDate1],member_id: params1[:member_id])
+					    rooms=Room.where(id:params[:roomsList])
+		                newBooking.rooms << rooms
+		            end     
+				end
+				context "when room are already booked" do
+						it"it should notify"do
+
+						end
+				end
 			end
 		end
 	end
