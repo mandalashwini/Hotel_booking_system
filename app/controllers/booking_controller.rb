0b9d7b2b2d117875a3e73db746a1b7488d7729d4
Ashwini Mandal <ashwini.mandal@gmail.com>
2018-02-21 20:20:16 +0530

    Error while changing to self

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -20,10 +20,11 @@ class BookingController < ApplicationController
  
   def setBookingStatus
    
-      readCache
-      @hotel_id=Room.select("hotel_id").where("id=?",@checkedRooms)
+     Booking_operations.readCache
+     
+      @hotel_id=Room.select("hotel_id").where("id=?",@@checkedRooms)
       convert_to_IntegerArray
-      @searchRoomExist=Room.joins(:bookings).where(:bookings=>{checkinDate: @checkinDate1,checkoutDate: @checkoutDate1},id:@roomsList).uniq
+      @searchRoomExist=Room.joins(:bookings).where(:bookings=>{checkinDate: @@checkinDate1,checkoutDate: @@checkoutDate1},id:@@roomsList).uniq
           if(@searchRoomExist.empty?)
                   create
                    bookingConfirmationMail
@@ -34,30 +35,17 @@ class BookingController < ApplicationController
          end
   end
 
-  def bookingConfirmationMail
-      UserMailer.booking_confirmation(Member.find(current_member.id)).deliver_now
-  end
+  
 
   def create
-      @newBooking=Booking.create(checkinDate: @checkinDate1.to_s,checkoutDate: @checkoutDate1.to_s,bookingDate: @bookingDate1.to_s,member_id: current_member.id)
-      @newBooking.rooms << @roomsList   
-      @hotelsList=@roomsList[0].hotel
+      @newBooking=Booking.create(checkinDate: @@checkinDate1.to_s,checkoutDate: @@checkoutDate1.to_s,bookingDate: @@bookingDate1.to_s,member_id: current_member.id)
+      @newBooking.rooms << @@roomsList   
+      @hotelsList=@@roomsList[0].hotel
                   
   end
 
-  def readCache
-      @checkedRooms=Rails.cache.read("rooms")
-      @checkinDate1 = Rails.cache.read("checkinDate")
-      @checkoutDate1 = Rails.cache.read("checkoutDate")
-      @bookingDate1 = Rails.cache.read("bookingDate")
-  end
-
-  def convert_to_IntegerArray
-      @roomsList=Array.new
-      @checkedRooms.each do |rooms|
-      @roomsList.push(Room.find(rooms.to_i))
-      end
-  end
+  
+  
 
 
 
@@ -66,19 +54,9 @@ class BookingController < ApplicationController
     bookingobj=Booking_operations.new
     @member=Member.find(current_member.id)
     @bookingDetails=@member.bookings.distinct
-   # puts "@bookingDetails".class
-        #@bookingDetails.each do |book|
-         #   @booking=book
-          #   @roomDetails=bookingobj.getRoomsDetails(@booking)
-      
-      #  end   # @hotelDetails=bookingobj.getHotelDetails
-       @bookingDetails=@bookingDetails.page(params[:page]).per(2)
+   @bookingDetails=@bookingDetails.page(params[:page]).per(2)
     end
    
 
-   def bookingDetails
-    puts "ooo",params.inspect
-   end
-
-
+   
  end
--- app/services/booking_operations.rb
+++ app/services/booking_operations.rb
@@ -1,13 +1,23 @@
 class Booking_operations
 
 	
-	
-		def getRoomsDetails(bookingDetails)
-			@roomDetails=Room.roomDetails(bookingDetails) 
-		end
 
-		def getHotelDetails
-			@hotelDetails=Hotel.hotelDetails(@roomDetails) 
-		end
-	 
+	 def self readCache
+      @@checkedRooms=Rails.cache.read("rooms")
+      @@checkinDate1 = Rails.cache.read("checkinDate")
+      @@checkoutDate1 = Rails.cache.read("checkoutDate")
+      @@bookingDate1 = Rails.cache.read("bookingDate")
+  end
+
+ def self.convert_to_IntegerArray
+      @@roomsList=Array.new
+      @@@checkedRooms.each do |rooms|
+      @@roomsList.push(Room.find(rooms.to_i))
+      end
+  end
+
+
+	def self.bookingConfirmationMail
+      UserMailer.booking_confirmation(Member.find(current_member.id)).deliver_now
+  end
 end
